65b2627cb79c212b1de9ee2520b26c8950f9e2c3
SalahEddine Akrim <salahakrim23@gmail.com>
2022-05-25 23:31:54 +0100

    fix some bugs on edit post

--- admin/edit_post.php
+++ admin/edit_post.php
@@ -20,17 +20,36 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     $post_date = $row['post_date'];
 }
 
+if (isset($_POST['update_post'])) {
+
+    $post_author = $_POST['post_author'];
+    $post_title = $_POST['post_title'];
+    $post_category_id = $_POST['post_category'];
+    $post_status = $_POST['post_status'];
+    $post_image = $_FILES['image']['name'];
+    $post_image_temp = $_FILES['image']['tmp_name'];
+    $post_content = $_POST['post_content'];
+    $post_tags = $_POST['post_tags'];
+
+    move_uploaded_file($post_image_temp, "../images/$post_image");
+
+    $query = "UPDATE post SET post_title = '{$post_title}', post_category_id = '{$post_category_id}', post_date = now(), post_author = '{$post_author}', post_status = '{$post_status}', post_tags = '{$post_tags}', post_content = '{$post_content}', post_image = '{$post_image}', WHERE post_id = $the_post_id ";
+
+    $update_post = mysqli_query($connection, $query);
+    confirmquery($update_post);
+}
+
 ?>
 
 
 <form action="" method="post" enctype="multipart/form-data">
     <div class="form-group">
         <label for="title">Post Title</label>
-        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="title">
+        <input value="<?php echo $post_title; ?>" type="text" class="form-control" name="post_title">
     </div>
     <div class="form-group">
 
-        <select name="" id="">
+        <select name="post_category" id="">
             <?php
             $query = "SELECT * FROM categories";
             $select_categories = mysqli_query($connection, $query);
@@ -49,7 +68,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
     <div class="form-group">
         <label for="title">Post Author</label>
-        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="author">
+        <input value="<?php echo $post_author; ?>" type="text" class="form-control" name="post_author">
     </div>
     <div class="form-group">
         <label for="post_status">Post Status</label>
@@ -57,6 +76,7 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
     </div>
     <div class="form-group">
         <img width="100" src="../Images/<?php echo $post_image; ?>" alt="">
+        <input type="file" name="image">
     </div>
     <div class="form-group">
         <label for="post_tags">Post Tags</label>
@@ -67,6 +87,6 @@ while ($row = mysqli_fetch_assoc($select_posts_by_id)) {
         <textarea class="form-control" name="post_content" id="" cols="30" rows="10"><?php echo $post_content; ?></textarea>
     </div>
     <div>
-        <input class="btn btn-primary" type="submit" name="create_post" value="Publish Post">
+        <input class="btn btn-primary" type="submit" name="update_post" value="Update Post">
     </div>
 </form>
